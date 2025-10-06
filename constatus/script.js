bea144a5d286447105242ddce8d68dddcceeee88
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-10-06 10:40:18 +0300

    Update script.js

--- constatus/script.js
+++ constatus/script.js
@@ -3,4 +3,4 @@ function  setText(t) { document.getElementById("status").textContent = t; };
 const url = new URL(window.location);
 const searchParams = url.searchParams;
 let w = searchParams.get('status');
-if w == '200' { setTitle("200, ok"); setText('{"status":"200","ok":"true"}') };
+if (w == '200') { setTitle("200, ok"); setText('{"status":"200","ok":"true"}') };
