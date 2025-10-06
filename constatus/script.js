7f527041193f8f665e672415d6989a50848c5d77
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-10-06 10:35:16 +0300

    Update script.js

--- constatus/script.js
+++ constatus/script.js
@@ -1,1 +1,6 @@
-
+function  setTitle(t) { document.getElementById("title").textContent = t }
+function  setText(t) { document.getElementById("text").textContent = t }
+const url = new URL(window.location);
+const searchParams = url.searchParams;
+let w = searchParams.get('status');
+if w == '200' {  }
