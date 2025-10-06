302bea92f99498c87cf705fa12e06e307d7852e2
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-10-06 10:38:58 +0300

    Update script.js

--- constatus/script.js
+++ constatus/script.js
@@ -1,6 +1,6 @@
-function  setTitle(t) { document.getElementById("title").textContent = t }
-function  setText(t) { document.getElementById("status").textContent = t }
+function  setTitle(t) { document.getElementById("title").textContent = t; };
+function  setText(t) { document.getElementById("status").textContent = t; };
 const url = new URL(window.location);
 const searchParams = url.searchParams;
 let w = searchParams.get('status');
-if w == '200' {  }
+if w == '200' { setTitle("200, ok"); setText('{"status":"200","ok":"true"}') };
