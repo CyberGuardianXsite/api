559bc4638aa8a44f834e0b33047ea4519ac59b8c
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-10-06 10:35:43 +0300

    Update script.js

--- constatus/script.js
+++ constatus/script.js
@@ -1,5 +1,5 @@
 function  setTitle(t) { document.getElementById("title").textContent = t }
-function  setText(t) { document.getElementById("text").textContent = t }
+function  setText(t) { document.getElementById("status").textContent = t }
 const url = new URL(window.location);
 const searchParams = url.searchParams;
 let w = searchParams.get('status');
