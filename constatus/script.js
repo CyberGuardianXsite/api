7574f99c996554da4606149a6d275010ef7bb576
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-10-17 23:30:42 +0300

    Update script.js

--- constatus/script.js
+++ constatus/script.js
@@ -1,6 +1,23 @@
-function  setTitle(t) { document.getElementById("title").textContent = t; };
-function  setText(t) { document.getElementById("status").textContent = t; };
+function  setTitle(t) {
+  document.getElementById("title").textContent = t;
+}
+function  setText(t) {
+  document.getElementById("status").textContent = t;
+}
 const url = new URL(window.location);
 const searchParams = url.searchParams;
 let w = searchParams.get('status');
-if (w == '200') { setTitle("200, ok"); setText('{"status":"200","ok":"true"}') };
+if (w == '200') {
+  setTitle("200, ok");
+  setText('{\"status\":\"200\",\"ok\":\"true\",\"robot\":\"false\"}')
+} else {
+  if (w == '403') {
+    setTitle("403")
+    setText("{\"status\":\"403\",\"ok\":\"false\",\"robot\":\"true\"}")
+  } else {
+    if (w == '503') {
+      setTitle("503")
+      setText("{\"status\":\"503\",\"ok\":\"false\",\"robot\":\"not\"}")
+    }
+  }
+}
